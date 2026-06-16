7707ddfb0802b7454f64bce846fcbd6ba9189c7f
nameczz <zizhao.chen@zilliz.com>
2026-06-16 15:34:48 +0800

    chore: bump version to v1.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="milvus_cli",
-    version="v1.2.1",
+    version="v1.2.2",
     author="Milvus Team",
     author_email="milvus-team@zilliz.com",
     url="https://github.com/zilliztech/milvus_cli",
